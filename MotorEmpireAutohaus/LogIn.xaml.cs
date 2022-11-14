668136cedc14271ec34282ab7a6079e47303d9d7
Rares <raresgabriel.musea@ulbsibiu.ro>
2022-11-14 15:20:24 +0200

    Fixed cross platform design across different platforms

--- MotorEmpireAutohaus/LogIn.xaml.cs
+++ MotorEmpireAutohaus/LogIn.xaml.cs
@@ -1,3 +1,4 @@
+using  Microsoft.Maui.Devices;
 namespace MotorEmpireAutohaus;
 
 public partial class LogIn : ContentPage
@@ -5,6 +6,32 @@ public partial class LogIn : ContentPage
 	public LogIn()
 	{
 		InitializeComponent();
+		ApplySpecificStyleDependingOnPlatform();
+	}
+
+
+	private void ApplySpecificStyleDependingOnPlatform()
+	{
+		if (DeviceInfo.Platform == DevicePlatform.WinUI)
+		{
+			AuthenticationLogo.HeightRequest = 320;
+			AuthenticationLogo.WidthRequest = 500;
+			emailEntry.HeightRequest = 25;
+			passwordEntry.HeightRequest = 25;
+			emailEntry.WidthRequest = 250;
+			passwordEntry.WidthRequest = 225;
+			passwordFrame.WidthRequest = 300;
+        }
+
+		if (DeviceInfo.Platform == DevicePlatform.Android)
+		{
+			emailEntry.HeightRequest = 45;
+			passwordEntry.HeightRequest = 45;
+			passwordEntry.WidthRequest = 225;
+			passwordFrame.WidthRequest = 300;
+			passwordShowButton.Margin = new Thickness(5);
+        }
+		
 	}
  
 	private void SetPasswordToVisible(object sender, EventArgs e)
---
+++ MotorEmpireAutohaus/Storage/DatabaseConfigurer.cs
@@ -0,0 +1,38 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorEmpireAutohaus.Storage
+{
+    public class DatabaseConfigurer
+    {
+        private readonly string _connectionString;
+
+        public bool ConnectionOpen { get;}
+        
+        public MySqlConnection? DatabaseConnection { get; set; }
+
+        public  DatabaseConfigurer()
+        {
+            ConnectionOpen = false;
+            _connectionString = @"server=localhost;userid=root;password=password;database=motor_empire_autohaus_database";
+            EstablishConnection();
+            
+        }
+
+        private async void EstablishConnection()
+        {
+            try
+            {
+                DatabaseConnection = new MySqlConnection(_connectionString);
+            }
+            catch (MySqlException mySqlEx)
+            {
+                await App.Current.MainPage.DisplayAlert("MotorEmpireAutohaus", $"ERROR!\n Could not establish connection with the database!\n Error details:{mySqlEx.Message}\n", "OK");
+            }
+        }
+    }
+}
